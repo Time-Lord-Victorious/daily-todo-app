218efd286505c34e768276e4a616a36270bcf417
Time-Lord-Victorious <junshenlee2017@gmail.com>
2024-12-28 21:33:56 +0800

    hooo

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -4,12 +4,12 @@ function ToDoList() {
 
 
 
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState(["Finish up plans on Noa tetralogy", "Complete todo app", "Prep for New Year's Day"]);
 
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event) {
-
+        setNewTask(event.target.value);
     }
     function addTask() {
 
@@ -27,9 +27,30 @@ function ToDoList() {
 
 
     return (
-        <>
+        <div className="to-do-list">
+            <h1>Daily Todo List</h1>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Enter ur task buddy!"
+                    value={newTask}
+                    onChange={handleInputChange}
+                />
+                <button className="add-button"
+                    onClick={addTask}>
+                    Add!
+                </button>
+            </div>
 
-        </>
+            <ol>
+                {tasks.map((task, index) =>
+
+                    <li key={index}>
+                        <span className="text">{task}</span>
+                    </li>
+                )}
+            </ol>
+        </div>
     )
 }
 export default ToDoList
